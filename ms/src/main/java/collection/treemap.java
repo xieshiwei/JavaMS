8c92e71a99d73b1a69d589802e42bff91dcda257
Neo.Xie <xswbbx@163.com>
2019-05-28 07:29:41 +0800

    treemap

---
+++ ms/src/main/java/collection/treemap.java
@@ -0,0 +1,75 @@
+package collection;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @ClassName treemap
+ * @Author 萌琪琪爸爸
+ * @Description //TODO
+ * @Date 2019/5/27 16:25
+ **/
+public class treemap {
+
+    /**
+     * 入口
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("---------------- 默认 排序结果-----------------");
+        createDefaultSortTreeMap();
+        System.out.println("---------------- 自定义 排序结果-----------------");
+        createDefinitionSortTreeMap();
+    }
+
+    /**
+     * TreeMap 默认排序
+     */
+    private static void createDefaultSortTreeMap() {
+        TreeMap<String, String> treeMap = new TreeMap<>();
+        init(treeMap);
+        print(treeMap);
+    }
+
+    /**
+     * 自定义排序
+     */
+    private static void createDefinitionSortTreeMap() {
+        TreeMap<String, String> treeMap = new TreeMap<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1);
+            }
+        });
+
+        init(treeMap);
+        print(treeMap);
+    }
+
+    /**
+     * map 初始化
+     *
+     * @param map
+     */
+    private static void init(Map<String, String> map) {
+        map.put("c", "1");
+        map.put("a", "1");
+        map.put("bb", "1");
+        map.put("d", "1");
+    }
+
+    /**
+     * 输出
+     *
+     * @param map
+     */
+    private static void print(Map<String, String> map) {
+        Iterator<Map.Entry<String, String>> it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, String> entry = it.next();
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+    }
+}
