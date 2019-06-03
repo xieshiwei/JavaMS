f173325cc23c68f244a0a237b007db53817d2992
Neo.Xie <xswbbx@163.com>
2019-06-03 11:43:46 +0800

    测试

---
+++ ms/src/main/java/JavaTest.java
@@ -0,0 +1,15 @@
+/**
+ * @ClassName JavaTest
+ * @Author 萌琪琪爸爸
+ * @Description //TODO
+ * @Date 2019/5/30 12:36
+ **/
+public class JavaTest {
+
+    public static void main(String[] args) {
+        String temp = "abcde";
+        int code = temp.hashCode();
+        System.out.println(code);
+    }
+
+}
