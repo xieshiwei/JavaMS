5a9e148e5e276f38c533b7b835002cc8b66e8a60
Neo.Xie <xswbbx@163.com>
2019-09-21 15:46:24 +0800

    springboot

---
+++ springbootms/src/main/java/com/java/springbootms/SpringbootmsApplication.java
@@ -0,0 +1,13 @@
+package com.java.springbootms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootmsApplication.class, args);
+    }
+
+}
