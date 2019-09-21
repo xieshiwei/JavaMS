8ff60897bd4838030ab0220e90fdebcc7e01047c
Neo.Xie <xswbbx@163.com>
2019-09-21 09:29:27 +0800

    quicksort test

--- ms/src/main/java/SF/sort/TestQuickSort.java
+++ ms/src/main/java/SF/sort/TestQuickSort.java
@@ -4,38 +4,44 @@ import java.util.Arrays;
 
 public class TestQuickSort {
 
-
-    public static void main(String[] args) {
-        int[] nums = {49, 38, 65, 97, 23, 22, 76, 1, 5, 8, 2, 0, -1, 22};
-        System.out.println("排序之前的数组为...............");
-        System.out.println(Arrays.toString(nums));
-        System.out.println("排序之后的数组为...............");
-        quitSort(0, nums.length - 1, nums);
-        System.out.println(Arrays.toString(nums));
-    }
-
-    public static void quitSort(int low, int high, int[] arr) {
-
-        if (low < high) {
+    /**
+     * 定义一个基准数
+     * 从右向左循环排序
+     * 从左向右循环排序
+     *
+     * @param low
+     * @param high
+     * @param arr
+     */
+    public static void quickSort(int low, int high, int[] arr) {
+        if (arr != null && arr.length > 0 && low < high) {
+            //获取基准数
             int index = getIndex(low, high, arr);
-            quitSort(low, index - 1, arr);
-            quitSort(index + 1, high, arr);
+            //从左往右
+            quickSort(low, index - 1, arr);
+            //从右往左
+            quickSort(index + 1, high, arr);
         }
     }
 
+    /**
+     * 获取基准数下标
+     *
+     * @param low
+     * @param high
+     * @param arr
+     * @return
+     */
     public static int getIndex(int low, int high, int[] arr) {
-
-        //取当前第一位为基准数
+        if (arr == null || arr.length <= 0) {
+            return -1;
+        }
         int temp = arr[low];
-
         while (low < high) {
-            //right to left
             while (low < high && temp <= arr[high]) {
                 high--;
             }
             arr[low] = arr[high];
-
-            //left to right
             while (low < high && temp >= arr[low]) {
                 low++;
             }
@@ -43,6 +49,13 @@ public class TestQuickSort {
         }
         arr[low] = temp;
         return low;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {49, 38, 65, 97, 23, 22, 76, 1, 5, 8, 2, 0, -1, 22};
+        System.out.println(Arrays.toString(nums));
+        quickSort(0, nums.length - 1, nums);
+        System.out.println(Arrays.toString(nums));
     }
 
 
