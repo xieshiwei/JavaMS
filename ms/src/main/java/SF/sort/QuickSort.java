f6f00cb5d008f7d82981c9ac99891f5e2c0a0111
Neo.Xie <xswbbx@163.com>
2019-09-29 09:05:05 +0800

    排序

--- ms/src/main/java/SF/sort/QuickSort.java
+++ ms/src/main/java/SF/sort/QuickSort.java
@@ -15,11 +15,18 @@ public class QuickSort {
      * 快速排序,说白了就是给基准数据找其正确索引位置的过程.
      * 首先在数组中选择一个基准点
      * 然后分别从数组的两端扫描数组，设两个指示标志（low指向起始位置，high指向末尾)
-     *  首先从后半部分开始，如果发现有元素比该基准点的值小，就交换low和high位置的值
-     *  然后从前半部分开始扫秒，发现有元素大于基准点的值，就交换low和high位置的值
+     * 首先从后半部分开始，如果发现有元素比该基准点的值小，就交换low和high位置的值
+     * 然后从前半部分开始扫秒，发现有元素大于基准点的值，就交换low和high位置的值
      * 如此往复循环，直到low>=high,然后把基准点的值放到high这个位置。
      * 一次排序就完成了。以后采用递归的方式分别对前半部分和后半部分排序
      * 当前半部分和后半部分均有序时该数组就自然有序了。
+     * <p>
+     * <p>
+     * <p>
+     * <p>
+     * getIndex:
+     * 1.
+     *
      * @param args
      */
     public static void main(String[] args) {
@@ -33,6 +40,16 @@ public class QuickSort {
         System.out.println(Arrays.toString(nums));
     }
 
+    /**
+     * 快排
+     * 容易写错的地方：
+     * 1.先判断low小于high
+     * 2.index-1不能写错index--
+     *
+     * @param arr
+     * @param low
+     * @param high
+     */
     public void quickSort(int[] arr, int low, int high) {
         if (low < high) {
             int index = getIndex(arr, low, high);
@@ -45,6 +62,11 @@ public class QuickSort {
 
     /**
      * 获取基准数下标
+     * 容易写错的地方：
+     * 1.最大值循环的时候，循环完后要把high的值赋值给low的位置
+     * 2.最小值循环的时候，循环完后要把low的值赋值给high的位置
+     * 3.两个while中的大小判断不要写错
+     *
      * @param nums
      * @param low
      * @param high
--- ms/src/main/java/SF/sort/TestQuickSort.java
+++ ms/src/main/java/SF/sort/TestQuickSort.java
@@ -4,14 +4,43 @@ import java.util.Arrays;
 
 public class TestQuickSort {
 
+    /**
+     * 快排：
+     * 1.找到一个基准点。
+     * 2.分别从数组的两段扫描数组，设置两个标记分别表示头部和尾部。
+     * 3.从后半部分开始扫描，如果发现有元素比该基准值小，就交换low和high位置的值。
+     * 4.从前半部分开始扫描，如果有元素大于基准值的值，就交换low和high位置的值。
+     * 5.扫描直到low>=high，然后把基准点的值放到low的位置，第一次排序就完成了。
+     * 6.采用递归的方式分别对前半部分，后半部分排序。
+     *
+     * @param low
+     * @param high
+     * @param arr
+     */
+    public static void quickSort(int low, int high, int[] arr) {
+        if (arr != null && arr.length > 0 && low < high) {
+            int index = getIndex(low, high, arr);
+            quickSort(low, index - 1, arr);
+            quickSort(index + 1, high, arr);
+        }
+    }
+
+    /**
+     * 返回基准数
+     *
+     * @param low
+     * @param high
+     * @param arr
+     * @return
+     */
     public static int getIndex(int low, int high, int[] arr) {
+
         int index = arr[low];
         while (low < high) {
             while (low < high && index <= arr[high]) {
                 high--;
             }
             arr[low] = arr[high];
-
             while (low < high && index >= arr[low]) {
                 low++;
             }
@@ -21,18 +50,11 @@ public class TestQuickSort {
         return low;
     }
 
-    public static void quickSort(int low, int high, int[] arr) {
-        if (arr != null && arr.length > 0 && low < high) {
-            int index = getIndex(low, high, arr);
-            quickSort(low, index - 1, arr);
-            quickSort(index + 1, high, arr);
-        }
-    }
-
     public static void main(String[] args) {
-        int[] arr = {3, 1, -2, 100, 86, 99, 34, 45};
+        int[] arr = {3, 1, 6, 5, 9, 100, 120, 0, -1};
         quickSort(0, arr.length - 1, arr);
         System.out.println(Arrays.toString(arr));
     }
+
 
 }
