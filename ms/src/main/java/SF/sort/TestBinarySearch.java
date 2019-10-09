2af5d6d8be57c615426f8328638e25ad6c84c652
Neo.Xie <xswbbx@163.com>
2019-10-09 08:58:44 +0800

    排序

--- ms/src/main/java/SF/sort/TestBinarySearch.java
+++ ms/src/main/java/SF/sort/TestBinarySearch.java
@@ -8,36 +8,5 @@ package SF.sort;
  **/
 public class TestBinarySearch {
 
-    /**
-     * 下标
-     *
-     * @param arr
-     * @param index
-     * @return
-     */
-    public static int getIndex(int[] arr, int index) {
-        if (arr == null || arr.length <= 0) {
-            return -1;
-        }
-        int low = 0;
-        int high = arr.length - 1;
-        int middle = 0;
-        while (low <= high) {
-            middle = (low + high) / 2;
-            if (arr[middle] > index) {
-                high = middle - 1;
-            } else if (arr[middle] < index) {
-                low = middle + 1;
-            } else {
-                return middle;
-            }
-        }
-        return -1;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {1, 3, 5, 7, 9};
-        System.out.println(getIndex(arr, 1));
-    }
 
 }
--- ms/src/main/java/SF/sort/TestQuickSort.java
+++ ms/src/main/java/SF/sort/TestQuickSort.java
@@ -8,36 +8,5 @@ import java.util.Arrays;
  */
 public class TestQuickSort {
 
-    public static void quicSort(int low, int high, int[] arr) {
-        if (low < high) {
-            int index = getIndex(low, high, arr);
-            quicSort(low, index - 1, arr);
-            quicSort(index + 1, high, arr);
-        }
-    }
-
-
-    private static int getIndex(int low, int high, int[] arr) {
-        int index = arr[low];
-        while (low < high) {
-            while (low < high && arr[high] >= index) {
-                high--;
-            }
-            arr[low] = arr[high];
-            while (low < high && arr[low] <= index) {
-                low++;
-            }
-            arr[high] = arr[low];
-        }
-        arr[low] = index;
-        return low;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {4, 2, 67, 44, 2, 7, 2, 6, 9, -1};
-        quicSort(0, arr.length - 1, arr);
-        System.out.println(Arrays.toString(arr));
-    }
-
 
 }
