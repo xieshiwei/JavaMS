f61c95439c52943ca2f0a8f8736bda1d18c190dd
Neo.Xie <xswbbx@163.com>
2019-12-25 00:41:24 +0800

    ok

---
+++ Netty/nettyporject/src/main/java/com/netty/dubborpc/customer/ClientBootstrap.java
@@ -0,0 +1,32 @@
+package com.netty.dubborpc.customer;
+
+import com.netty.dubborpc.netty.NettyClient;
+import com.netty.dubborpc.publicinterface.HelloService;
+
+/**
+ * @ClassName ClientBootstrap
+ * @Author 萌琪琪爸爸
+ * @Description //TODO
+ * @Date 2019/12/24 21:15
+ **/
+public class ClientBootstrap {
+
+    //协议头
+    public static final String providerName = "HelloService#hello#";
+
+
+    public static void main(String[] args) throws Exception {
+
+        //创建一个消费者
+        NettyClient customer = new NettyClient();
+
+        //创建代理对象
+        HelloService service = (HelloService) customer.getBean(HelloService.class, providerName);
+
+        //通过代理对象调用服务提供者的方法（服务）
+        String res = service.hello("你好 dubbo");
+        System.out.println("调用的结果 res=" + res);
+
+
+    }
+}
